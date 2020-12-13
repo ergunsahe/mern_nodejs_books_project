c22a7b746a0c9f0c605fac6855130d82d15e601e
ergunsahe <66415270+ergunsahe@users.noreply.github.com>
2020-12-13 22:51:54 +0100

    change port value for testing heroku error handling

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`I'm listening on port ${port}`);
 });
