e0e095fcde27f5c639ae887151736f322ca2c4e6
ergunsahe <66415270+ergunsahe@users.noreply.github.com>
2020-12-14 21:27:48 +0100

    fix connect url

--- models/connectDB.js
+++ models/connectDB.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
+    await mongoose.connect(process.env.MONGODB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
